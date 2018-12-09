20333c267f028eec9942311277a5f6522e68b5f8
anmrtz <anmrtz>
2018-12-08 22:24:32 -0700

    Forwarding table type refactoring

--- emulator.cpp
+++ emulator.cpp
@@ -14,9 +14,9 @@ namespace po = boost::program_options;
 using TransportPacket = net::TransportPacket;
 using TRANSPORT_PRIORITY = TransportPacket::TRANSPORT_PRIORITY;
 
-static forwarding_table parse_forwarding_table(std::iostream & stream)
+static emulator_table parse_emulator_table(std::iostream & stream)
 {
-    forwarding_table ft;
+    emulator_table ft;
     std::string line;
 
     while(std::getline(stream,line))
@@ -26,7 +26,7 @@ static forwarding_table parse_forwarding_table(std::iostream & stream)
         name_t name;
         port_t port;
 
-        forwarding_entry fe;
+        emulator_entry ee;
 
         std::stringstream ss(line);
         try
@@ -35,43 +35,43 @@ static forwarding_table parse_forwarding_table(std::iostream & stream)
             // get emulator
             ss >> name;
             ss >> str; port = boost::lexical_cast<port_t>(str);
-            fe.emulator = std::make_pair(name,port);
+            ee.emulator = std::make_pair(name,port);
 
             // get destination
             ss >> name;
             ss >> str; port = boost::lexical_cast<port_t>(str);
-            fe.destination = std::make_pair(name,port);
+            ee.destination = std::make_pair(name,port);
             
             // get namehop
             ss >> name;
             ss >> str; port = boost::lexical_cast<port_t>(str);
-            fe.next_hop = std::make_pair(name,port);
+            ee.next_hop = std::make_pair(name,port);
 
             // get delay
-            ss >> str; fe.delay = decltype(fe.delay)(boost::lexical_cast<uint32_t>(str));
+            ss >> str; ee.delay = decltype(ee.delay)(boost::lexical_cast<uint32_t>(str));
 
             // get loss probability
-            ss >> str; fe.loss_probability = boost::lexical_cast<uint16_t>(str);
+            ss >> str; ee.loss_probability = boost::lexical_cast<uint16_t>(str);
         }
         catch (std::exception & e)
         {
             throw std::runtime_error("parse_forwarding_table error. Could not parse line: " + line + "; " + e.what() + "\n");
         }
 
-        ft.emplace_back(std::move(fe));
+        ft.emplace_back(std::move(ee));
     }
     return ft;
 }
 
-static void print_forwarding_table(const forwarding_table & ft, std::ostream & stream)
+static void print_forwarding_table(const emulator_table & ft, std::ostream & stream)
 {
-    for (const auto & fe : ft)
+    for (const auto & ee : ft)
     {
-        stream << "Emulator(" << fe.emulator.first << "," << fe.emulator.second << ") ";
-        stream << "Destination(" << fe.destination.first << "," << fe.destination.second << ") ";
-        stream << "NextHop(" << fe.next_hop.first << "," << fe.next_hop.second << ") ";
-        stream << "Delay-ms(" << fe.delay.count() << ") ";
-        stream << "Loss-\%(" << uint16_t(fe.loss_probability) << ")\n";
+        stream << "Emulator(" << ee.emulator.first << "," << ee.emulator.second << ") ";
+        stream << "Destination(" << ee.destination.first << "," << ee.destination.second << ") ";
+        stream << "NextHop(" << ee.next_hop.first << "," << ee.next_hop.second << ") ";
+        stream << "Delay-ms(" << ee.delay.count() << ") ";
+        stream << "Loss-\%(" << +ee.loss_probability << ")\n";
     }
 }
 
@@ -91,7 +91,7 @@ static packet_queue outgoing_queue(tp_left_priority_less_than);
 int main(int argc, char * argv[])
 {
     uint16_t emulator_port, queue_size;
-    std::string forwarding_table_filename, log_filename;
+    std::string emulator_table_filename, log_filename;
 
     {
         po::options_description desc;
@@ -100,8 +100,8 @@ int main(int argc, char * argv[])
             desc.add_options()
                 (",p", po::value<decltype(emulator_port)>(&emulator_port)->required(), "emulator port")
                 (",q", po::value<decltype(queue_size)>(&queue_size)->required(), "queue size")
-                (",f", po::value<decltype(forwarding_table_filename)>(&forwarding_table_filename)->required(), 
-                    "forwarding table filename")
+                (",f", po::value<decltype(emulator_table_filename)>(&emulator_table_filename)->required(), 
+                    "emulator table filename")
                 (",l", po::value<decltype(log_filename)>(&log_filename)->required(), "log filename");
 
             po::variables_map vm;
@@ -122,15 +122,15 @@ int main(int argc, char * argv[])
         return -1;
     }
 
-    std::fstream ft_file(forwarding_table_filename);
-    if (!ft_file.is_open())
+    std::fstream et_file(emulator_table_filename);
+    if (!et_file.is_open())
     {
-        std::cerr << "Could not open forwarding table file: " << forwarding_table_filename << '\n';
+        std::cerr << "Could not open forwarding table file: " << emulator_table_filename << '\n';
         return -1;
     }
     try
     {
-        auto ft = parse_forwarding_table(ft_file);
+        auto ft = parse_emulator_table(et_file);
         std::cout << "Parsed forwarding table:\n"; print_forwarding_table(ft,std::cout);
     }
     catch (std::exception & e)
--- emulator.hpp
+++ emulator.hpp
@@ -5,9 +5,10 @@
 #include <vector>
 #include <utility>
 #include <chrono>
+#include <map>
 
 using host_port = std::pair<std::string,uint16_t>; // <hostname,port>
-struct forwarding_entry
+struct emulator_entry
 {
     host_port emulator;
     host_port destination;
@@ -16,4 +17,19 @@ struct forwarding_entry
     std::chrono::milliseconds delay;
     uint8_t loss_probability; // [0,100]
 };
-using forwarding_table = std::vector<forwarding_entry>;
+using emulator_table = std::vector<emulator_entry>;
+
+struct forwarding_dest
+{
+    sockaddr dest_addr;
+    uint32_t distance;
+
+    std::chrono::milliseconds delay;
+    uint8_t loss_probability; // [0,100]
+
+    forwarding_dest(const sockaddr & addr, uint32_t distance_ = 0, 
+        const std::chrono::milliseconds & delay_ = std::chrono::milliseconds(0), uint8_t loss_probability_ = 0) :
+        dest_addr(addr), distance(distance_), delay(delay_), loss_probability(loss_probability_)
+    {}
+};
+using forwarding_table = std::map<uint32_t, forwarding_dest>; // ip4 destination, gateway emulator; process from forwarding_table
