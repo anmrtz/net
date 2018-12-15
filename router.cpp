2ba8db161b5e00acce1949dfc30132d1f6367351
anmrtz <anmrtz>
2018-12-14 20:42:01 -0700

    Initial router functions

--- router.cpp
+++ router.cpp
@@ -12,13 +12,40 @@
 
 #include <boost/program_options.hpp>
 #include <boost/lexical_cast.hpp>
-/*
+
+namespace po = boost::program_options;
+using TransportPacket = net::TransportPacket;
+
+using packet_queue = std::queue<TransportPacket>;
+static packet_queue outgoing_queue;
+using topology = int;
+
+const auto sockaddr_in_comp = [](const sockaddr_in & left, const sockaddr_in & right) -> bool
+{
+    if (left.sin_addr.s_addr < right.sin_addr.s_addr)
+        return true;
+    else if (left.sin_addr.s_addr == right.sin_addr.s_addr)
+        if (left.sin_port < right.sin_port)
+            return true;
+    return false;
+};
+using forwarding_table = std::map<sockaddr_in, sockaddr_in, decltype(sockaddr_in_comp)>; // destination, gateway emulator; process from forwarding_table
+
+topology generate_topology(std::iostream & stream)
+{
+
+    return {};
+}
+
+void print_topology(const topology & top, std::ostream & stream)
+{
+
+}
+
 static void routing_loop(const forwarding_table & ft, const net::sock_fd & recv_sock_fd, const sockaddr & router_addr,
     const uint32_t queue_max_size)
 {
     std::array<uint8_t, net::RECV_BUFFER_SIZE> recv_buf;
-    auto packet_wake_time = std::chrono::system_clock::now();
-    bool front_packet_being_delayed{false};
 
     std::cout << "\nStarting routing loop...\n";
 #ifdef DEBUG_MSG
@@ -40,66 +67,92 @@ static void routing_loop(const forwarding_table & ft, const net::sock_fd & recv_
 
             // Future plans - check if this is a routing packet
 
-            // else try to queue it
-            if (outgoing_queue.size() < queue_max_size)
-            {
-                // first make sure the packet has a valid next-hop for its destination
-                const sockaddr packet_dest_addr = recv_packet.get_transport_dest();
+            // else route it
+            // first make sure the packet has a valid next-hop for its destination
+            const sockaddr packet_dest_addr = recv_packet.get_transport_dest();
 
-                if (ft.count(*(sockaddr_in*)&packet_dest_addr))
-                    outgoing_queue.push(std::move(recv_packet));
-                else
-                    std::cout << "routing_loop event: packet dropped due to no valid next-hop\n";
-            }
+            if (ft.count(*(sockaddr_in*)&packet_dest_addr))
+                outgoing_queue.push(std::move(recv_packet));
             else
-            {
-                std::cout << "routing_loop event: packet dropped due to full queue\n";
-            }
+                std::cout << "routing_loop event: packet dropped due to no valid next-hop\n";
         }
         // else if there is a packet being delayed and the delay is over
-        else if (front_packet_being_delayed && std::chrono::system_clock::now() > packet_wake_time)
+        else if (!outgoing_queue.empty())
         {
             // get forwarding hop
             const auto & front_packet = outgoing_queue.front();
             const sockaddr front_dest_addr = front_packet.get_transport_dest();
             const auto & forward_hop = ft.at(*(sockaddr_in*)&front_dest_addr);
 
-            const auto drop_chance = forward_hop.loss_probability;
-            decltype(drop_chance) roll_percentage{random_percent()};
 #ifdef DEBUG_MSG
             const auto send_time = std::chrono::system_clock::now() - emulator_start_time;
             std::cout << "Packet. Type : " 
                 << static_cast<std::underlying_type<net::BASE_PACKET_TYPE>::type>(front_packet.get_base_type())
                 << "; Orig: " << net::sockaddr_to_str(front_packet.get_transport_src()) 
                 << "; Dest: " << net::sockaddr_to_str(front_packet.get_transport_dest())
-                << "; Hop: " << net::sockaddr_to_str(*(sockaddr*)&forward_hop.hop_addr)
+                << "; Hop: " << net::sockaddr_to_str(*(sockaddr*)&forward_hop)
                 << "; Time (ms): " << std::chrono::duration_cast<std::chrono::milliseconds>(send_time).count()
-                << "; SN: " << front_packet.get_seq_no();
+                << "; SN: " << front_packet.get_seq_no()
+                << '\n';
 #endif
-            if (roll_percentage >= drop_chance)
-            {
-                front_packet.forward_packet(recv_sock_fd,*(sockaddr*)&forward_hop.hop_addr);
-            }
-#ifdef DEBUG_MSG
-            else
-                std::cout << " - DROPPED";
-            std::cout << '\n';
-#endif
+            front_packet.forward_packet(recv_sock_fd,*(sockaddr*)&forward_hop);
             outgoing_queue.pop();
-            front_packet_being_delayed = false;
-        }
-        // else retrieve the next packet if there 
-        else if (!front_packet_being_delayed && !outgoing_queue.empty())
-        {
-            const sockaddr front_dest_addr = outgoing_queue.front().get_transport_dest();
-            packet_wake_time = std::chrono::system_clock::now() + ft.at(*(sockaddr_in*)&front_dest_addr).delay;
-            //std::cout << "Delaying packet (ms): " << std::chrono::duration_cast<std::chrono::milliseconds>(packet_wake_time - std::chrono::system_clock::now()).count();
-            front_packet_being_delayed = true;
         }
     }
 }
-*/
+
 int main(int argc, char * argv[])
 {
+    uint16_t emulator_port;
+    std::string topology_filename;
 
+    {
+        po::options_description desc;
+        try
+        {
+            desc.add_options()
+                (",p", po::value<decltype(emulator_port)>(&emulator_port)->required(), "emulator port")
+                (",f", po::value<decltype(topology_filename)>(&topology_filename)->required(), 
+                    "topology filename");
+
+            po::variables_map vm;
+
+            po::store(po::parse_command_line(argc,argv,desc), vm);
+            po::notify(vm);
+        }
+        catch (po::error& e)
+        {
+            std::cerr << "Option parse error: " << e.what() << '\n' << desc << '\n';
+            return -1;
+        }
+    }
+
+    if (emulator_port < 1024)
+    {
+        std::cerr << "Emulator port must be >= 1024\n";
+        return -1;
+    }
+
+    try
+    {
+        std::fstream top_file(topology_filename);
+        if (!top_file.is_open())
+        {
+            std::cerr << "Could not open forwarding table file: " << topology_filename << '\n';
+            return -1;
+        }
+
+        const auto recv_fd_addr = net::bind_recv_local(emulator_port);
+        auto top = generate_topology(top_file);
+        std::cout << "\nGenerated forwarding table for this node:\n"; print_topology(top,std::cout);
+
+        //routing_loop(top,recv_fd_addr.first,recv_fd_addr.second);
+    }
+    catch (std::runtime_error & e)
+    {
+        std::cerr << "\nError generating forwarding table: " << e.what() << '\n' << std::flush;
+        return -1;
+    }
+
+    return 0;
 }
