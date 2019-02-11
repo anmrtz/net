0b2469e5cf6d724a7a38d4e9b4c95ad13c632e31
anmrtz <anmrtz>
2019-02-10 18:05:12 -0700

    Initial forwarding table generation from shortest paths

--- router.cpp
+++ router.cpp
@@ -45,7 +45,6 @@ typedef boost::adjacency_list < setS, vecS, undirectedS, vtx_prop, boost::proper
 typedef boost::graph_traits<topology_t>::edge_parallel_category disallow_parallel_edge_tag;
 typedef boost::graph_traits < topology_t >::vertex_descriptor vtx_desc;
 typedef boost::graph_traits < topology_t >::edge_descriptor edge_desc;
-typedef std::pair<int, int> edge_t;
 
 struct forwarding_hop
 {
@@ -72,15 +71,15 @@ const auto sockaddr_in_comp = [](const sockaddr_in & left, const sockaddr_in & r
             return true;
     return false;
 };
-using forwarding_table = std::map<sockaddr_in, forwarding_hop, decltype(sockaddr_in_comp)>; // destination, gateway emulator; process from forwarding_table
-using topology_nodes = std::map<sockaddr_in, int, decltype(sockaddr_in_comp)>;
+//using forwarding_table = std::map<sockaddr_in, forwarding_hop, decltype(sockaddr_in_comp)>; // destination, gateway emulator; process from forwarding_table
+using forwarding_table = std::map<sockaddr_in, sockaddr_in, decltype(sockaddr_in_comp)>;
 using topology_links = std::multimap<sockaddr_in, sockaddr_in, decltype(sockaddr_in_comp)>;
 
 // topology and this-node reference states
-topology_nodes top_nodes(sockaddr_in_comp);
 topology_links top_links(sockaddr_in_comp);
 sockaddr_in this_node_addr;
-std::set<sockaddr_in> adjacent_nodes;
+using adjacent_nodes_t = std::set<sockaddr_in, decltype(sockaddr_in_comp)>;
+adjacent_nodes_t adjacent_nodes(sockaddr_in_comp);
 topology_t top; // const topology graph
 
 // running topology states
@@ -88,22 +87,121 @@ struct node_state
 {
     uint32_t last_recv_seq_no{0};
     std::chrono::system_clock::time_point last_heartbeat_time;
-    bool is_active{false};
+    bool is_active{true};
+    int node_id{-1};
 };
-std::map<sockaddr_in, node_state > node_states;
-std::mutex node_state_mtx, socket_mtx;
+using node_states_t = std::map<sockaddr_in, node_state, decltype(sockaddr_in_comp)>; 
+node_states_t node_states(sockaddr_in_comp);
+std::mutex socket_mtx;
 
-std::set<sockaddr_in> get_adj_nodes(const topology_t & top, const sockaddr_in & node)
+// generate a set of all nodes adjacent to the specified node
+static adjacent_nodes_t get_adj_nodes(const sockaddr_in & node)
 {
-    std::set<sockaddr_in> an;
+    adjacent_nodes_t an(sockaddr_in_comp);
 
+    for (const auto & link : top_links)
+    {
+        if (link.first == node)
+        {
+            an.insert(link.second);
+        }
+        else if (link.second == node)
+        {
+            an.insert(link.first);
+        }
+    }
 
+    return an;
 }
 
-topology_t generate_topology(std::iostream & stream)
+static topology_t generate_topology()
 {
     topology_t top;
 
+    for (auto & node : node_states)
+    {
+        auto vtx = boost::add_vertex(top);
+        top[vtx].node_addr = node.first;
+        node.second.node_id = vtx;
+    }
+    for (const auto & link : top_links)
+    {
+        auto l_node = node_states.at(link.first);
+        auto r_node = node_states.at(link.second);
+
+        if (l_node.is_active && r_node.is_active)
+            boost::add_edge(l_node.node_id,r_node.node_id,1,top);
+    }
+
+    return top;
+}
+
+static void print_forwarding_table(const forwarding_table & ft, std::ostream & stream)
+{
+    for (const auto & fe : ft)
+    {
+        sockaddr_in * dest_addr = (sockaddr_in*)&fe.first;
+        sockaddr_in * next_hop_addr = (sockaddr_in*)&fe.second;
+#ifdef DEBUG_MSG
+        stream << "Destination: (" << inet_ntoa(dest_addr->sin_addr) << ',' <<  ntohs(dest_addr->sin_port) << 
+            ") --> Next-hop: (" << inet_ntoa(next_hop_addr->sin_addr) << ',' << ntohs(next_hop_addr->sin_port) << ")\n";
+#endif
+    }
+}
+
+static forwarding_table generate_forwarding_table(const sockaddr_in & source_node, const topology_t & top)
+{
+    forwarding_table ft(sockaddr_in_comp);
+
+    // calculate sample shortest path data
+    std::vector<vtx_desc> parents(boost::num_vertices(top));
+    std::vector<int> distances(boost::num_vertices(top));
+    boost::dijkstra_shortest_paths(top,node_states.at(source_node).node_id, boost::predecessor_map(&parents[0]).distance_map(&distances[0]));
+
+    // Output results
+    typedef boost::graph_traits<topology_t>::vertex_iterator VItr;
+    VItr vitr, vend;
+    std::cout << "Dijkstra output from node " << node_states.begin()->second.node_id << " :\n";
+    for (boost::tie(vitr, vend) = boost::vertices(top); vitr != vend; ++vitr) 
+    {
+        std::string node_name = net::sockaddr_to_str(top[*vitr].node_addr);
+
+        std::cout << "distance(" << node_name << ") = " << distances[*vitr] << ", ";
+        std::cout << "parent(" << node_name << ") = " << top[parents[*vitr]].node_addr << '\n';
+    }
+    std::cout << '\n';
+
+    std::cout << "Generating forwarding paths from Dijkstra shortest paths...\n";
+    for (const auto & node : node_states)
+    {
+        if (!node.second.is_active || node.first == this_node_addr)
+            continue;
+
+        auto src_idx = node_states.at(this_node_addr).node_id;
+        // trace path
+        std::deque<decltype(src_idx)> path_to;
+        // get immediate parent
+        decltype(src_idx) curr_idx = node.second.node_id;
+        path_to.push_front(curr_idx);
+        while (curr_idx != src_idx)
+        {
+            decltype(curr_idx) parent_of_curr = parents[curr_idx];
+            path_to.push_front(parent_of_curr);
+            curr_idx = parent_of_curr;
+            std::cout << "Loop " << "cidx: " << curr_idx << "sidx: " << src_idx << "\n";
+        }
+
+        ft.insert(std::make_pair(node.first,top[path_to.at(1)].node_addr));
+    }
+
+    std::cout << "Printing forwarding table:\n";
+    print_forwarding_table(ft,std::cout);
+
+    return ft;
+}
+
+topology_t parse_topology(std::iostream & stream)
+{
     // get vertices
     std::string line;
 
@@ -126,9 +224,13 @@ topology_t generate_topology(std::iostream & stream)
                 sockaddr_in block_addr = net::get_sockaddr_in_from_hostport(ip_port.at(0),boost::lexical_cast<uint16_t>(ip_port.at(1)));
                 if (!first_node_found)
                     first_node_addr = block_addr;
-                
-                if (!top_nodes.count(block_addr))
-                    top_nodes.insert(std::make_pair(block_addr,-1));
+
+                if (!node_states.count(block_addr))
+                {
+                    node_state new_node;
+                    new_node.last_heartbeat_time = std::chrono::system_clock::now();
+                    node_states.insert(std::make_pair(block_addr,new_node));
+                }
 
                 if (first_node_found)
                 {
@@ -149,17 +251,10 @@ topology_t generate_topology(std::iostream & stream)
             throw std::runtime_error("generate_topology error. Could not parse line: " + line + "; " + e.what() + "\n");
         }
     }
+    if (!node_states.count(this_node_addr))
+        throw std::runtime_error("Error: localhost addr - not found in topology file\n");
 
-    for (auto & node : top_nodes)
-    {
-        auto vtx = boost::add_vertex(top);
-        top[vtx].node_addr = node.first;
-        node.second = vtx;
-    }
-    for (const auto & link : top_links)
-    {
-        boost::add_edge(top_nodes.at(link.first),top_nodes.at(link.second),1,top);
-    }
+    auto top = generate_topology();
 
     // debug graph data
     typedef boost::graph_traits<topology_t>::vertex_iterator VItr;
@@ -185,21 +280,7 @@ topology_t generate_topology(std::iostream & stream)
     std::ofstream dotfile("top.dot");    
     write_graphviz (dotfile, top, make_label_writer(&node_names[0])); // output topology to dot file
 
-    // calculate sample shortest path data
-    std::vector<vtx_desc> parents(boost::num_vertices(top));
-    std::vector<int> distances(boost::num_vertices(top));
-    boost::dijkstra_shortest_paths(top,top_nodes.begin()->second, boost::predecessor_map(&parents[0]).distance_map(&distances[0]));
-
-    // Output results
-    std::cout << "Dijkstra output from node " << top_nodes.begin()->first << " :\n";
-    for (boost::tie(vitr, vend) = boost::vertices(top); vitr != vend; ++vitr) 
-    {
-        std::string node_name = net::sockaddr_to_str(top[*vitr].node_addr);
-
-        std::cout << "distance(" << node_name << ") = " << distances[*vitr] << ", ";
-        std::cout << "parent(" << node_name << ") = " << top[parents[*vitr]].node_addr << '\n';
-    }
-    std::cout << '\n';
+    generate_forwarding_table(this_node_addr,top);
 
     return top;
 }
@@ -207,7 +288,7 @@ topology_t generate_topology(std::iostream & stream)
 static void print_topology(const topology_t & top, std::ostream & stream)
 {
     std::vector<std::string> node_names;
-    for (const auto & node : top_nodes)
+    for (const auto & node : node_states)
         node_names.push_back(net::sockaddr_to_str(node.first));
 
     write_graphviz (stream, top, make_label_writer(&node_names[0]));
@@ -307,14 +388,14 @@ static void routing_loop(const forwarding_table & ft, const net::sock_fd & recv_
                 << static_cast<std::underlying_type<net::BASE_PACKET_TYPE>::type>(front_packet.get_base_type())
                 << "; Orig: " << net::sockaddr_to_str(front_packet.get_transport_src()) 
                 << "; Dest: " << net::sockaddr_to_str(front_packet.get_transport_dest())
-                << "; Hop: " << net::sockaddr_to_str(*(sockaddr*)&forward_hop.hop_addr)
+                << "; Hop: " << net::sockaddr_to_str(*(sockaddr*)&forward_hop)
                 << "; Time (ms): " << std::chrono::duration_cast<std::chrono::milliseconds>(send_time).count()
                 << "; SN: " << front_packet.get_seq_no()
                 << '\n';
 #endif
             {
                 std::lock_guard<std::mutex> lock(socket_mtx);
-                front_packet.forward_packet(recv_sock_fd,*(sockaddr*)&forward_hop.hop_addr);
+                front_packet.forward_packet(recv_sock_fd,*(sockaddr*)&forward_hop);
             }    
             outgoing_queue.pop();
         }
@@ -362,8 +443,10 @@ int main(int argc, char * argv[])
             return -1;
         }
 
-        const auto recv_fd_addr = net::bind_recv_local(emulator_port);
-        auto top = generate_topology(top_file);
+        //const auto recv_fd_addr = net::bind_recv_local(emulator_port);
+        this_node_addr = net::get_sockaddr_in_from_hostport("localhost",emulator_port);
+        std::cout << "Localhost found at: " << this_node_addr << "\n";
+        auto top = parse_topology(top_file);
         std::cout << "\nTopology for this node:\n"; print_topology(top,std::cout);
 
         //routing_loop(top,recv_fd_addr.first,recv_fd_addr.second);
