ff811840be6179a2a3f8701d32c3591cb2d0246a
anmrtz <anmrtz>
2019-02-10 21:04:59 -0700

    Heartbeat sending and topology/forwarding updates - initial implementation

--- router.cpp
+++ router.cpp
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <thread>
 #include <mutex>
+#include <future>
 
 #include <boost/program_options.hpp>
 #include <boost/lexical_cast.hpp>
@@ -29,11 +30,11 @@ static packet_queue outgoing_queue;
 using namespace boost;
 
 // Delay between sending node-active notifications to adjacent routers
-static constexpr std::chrono::milliseconds NODE_HEARTBEAT_INTERVAL{200};
+static constexpr std::chrono::milliseconds NODE_HEARTBEAT_INTERVAL{1000};
 static_assert(NODE_HEARTBEAT_INTERVAL.count() >= 0, "NODE_HEARTBEAT_INTERVAL must be >= 0\n");
 
 // Delay before pruning inactive nodes from the local topology state
-static constexpr std::chrono::milliseconds NODE_EXPIRATION_INTERVAL{500};
+static constexpr std::chrono::milliseconds NODE_EXPIRATION_INTERVAL{3000};
 static_assert(NODE_EXPIRATION_INTERVAL.count() >= 0, "NODE_EXPIRATION_INTERVAL must be >= 0\n");
 
 // Graph stuff
@@ -62,6 +63,10 @@ bool operator==(const sockaddr_in & left, const sockaddr_in & right)
         left.sin_addr.s_addr == right.sin_addr.s_addr &&
         left.sin_port == right.sin_port;
 }
+bool operator!=(const sockaddr_in & left, const sockaddr_in & right)
+{
+    return !(left == right);
+}
 const auto sockaddr_in_comp = [](const sockaddr_in & left, const sockaddr_in & right) -> bool
 {
     if (left.sin_addr.s_addr < right.sin_addr.s_addr)
@@ -71,9 +76,9 @@ const auto sockaddr_in_comp = [](const sockaddr_in & left, const sockaddr_in & r
             return true;
     return false;
 };
-//using forwarding_table = std::map<sockaddr_in, forwarding_hop, decltype(sockaddr_in_comp)>; // destination, gateway emulator; process from forwarding_table
-using forwarding_table = std::map<sockaddr_in, sockaddr_in, decltype(sockaddr_in_comp)>;
+using forwarding_table_t = std::map<sockaddr_in, sockaddr_in, decltype(sockaddr_in_comp)>;
 using topology_links = std::multimap<sockaddr_in, sockaddr_in, decltype(sockaddr_in_comp)>;
+forwarding_table_t ft(sockaddr_in_comp);
 
 // topology and this-node reference states
 topology_links top_links(sockaddr_in_comp);
@@ -95,10 +100,8 @@ node_states_t node_states(sockaddr_in_comp);
 std::mutex socket_mtx;
 
 // generate a set of all nodes adjacent to the specified node
-static adjacent_nodes_t get_adj_nodes(const sockaddr_in & node)
+static void get_adj_nodes(const sockaddr_in & node, adjacent_nodes_t & an)
 {
-    adjacent_nodes_t an(sockaddr_in_comp);
-
     for (const auto & link : top_links)
     {
         if (link.first == node)
@@ -111,7 +114,12 @@ static adjacent_nodes_t get_adj_nodes(const sockaddr_in & node)
         }
     }
 
-    return an;
+    std::cout << "Adjacent nodes:\n";
+    for (const auto & node : an)
+    {
+        std::cout << node << '\n';
+    }
+    std::cout << "--- End of adjacent nodes ---\n";
 }
 
 static topology_t generate_topology()
@@ -136,9 +144,9 @@ static topology_t generate_topology()
     return top;
 }
 
-static void print_forwarding_table(const forwarding_table & ft, std::ostream & stream)
+static void print_forwarding_table(const forwarding_table_t & ft_, std::ostream & stream)
 {
-    for (const auto & fe : ft)
+    for (const auto & fe : ft_)
     {
         sockaddr_in * dest_addr = (sockaddr_in*)&fe.first;
         sockaddr_in * next_hop_addr = (sockaddr_in*)&fe.second;
@@ -149,9 +157,9 @@ static void print_forwarding_table(const forwarding_table & ft, std::ostream & s
     }
 }
 
-static forwarding_table generate_forwarding_table(const sockaddr_in & source_node, const topology_t & top)
+static void update_forwarding_table(const sockaddr_in & source_node)
 {
-    forwarding_table ft(sockaddr_in_comp);
+    const auto top = generate_topology();
 
     // calculate sample shortest path data
     std::vector<vtx_desc> parents(boost::num_vertices(top));
@@ -196,8 +204,6 @@ static forwarding_table generate_forwarding_table(const sockaddr_in & source_nod
 
     std::cout << "Printing forwarding table:\n";
     print_forwarding_table(ft,std::cout);
-
-    return ft;
 }
 
 topology_t parse_topology(std::iostream & stream)
@@ -254,7 +260,7 @@ topology_t parse_topology(std::iostream & stream)
     if (!node_states.count(this_node_addr))
         throw std::runtime_error("Error: localhost addr - not found in topology file\n");
 
-    auto top = generate_topology();
+    const auto top = generate_topology();
 
     // debug graph data
     typedef boost::graph_traits<topology_t>::vertex_iterator VItr;
@@ -280,8 +286,6 @@ topology_t parse_topology(std::iostream & stream)
     std::ofstream dotfile("top.dot");    
     write_graphviz (dotfile, top, make_label_writer(&node_names[0])); // output topology to dot file
 
-    generate_forwarding_table(this_node_addr,top);
-
     return top;
 }
 
@@ -297,25 +301,44 @@ static void print_topology(const topology_t & top, std::ostream & stream)
 // update the known topology based on the received node state message (or expiration of node state)
 // update the forwarding table based on the changed topology
 // forward node state packets to neighbors
-static void process_node_state(const sockaddr & src_addr, uint32_t seq_no)
+static bool process_node_state(const sockaddr & src_addr, uint32_t seq_no)
 {
+    std::cout << "Recv heartbeat from: " << *(sockaddr_in*)&(src_addr) << "\n";
 
-}
+    auto & heartbeat_source = node_states.at(*(sockaddr_in*)&src_addr);
+    if (heartbeat_source.last_recv_seq_no < seq_no)
+    {
+        heartbeat_source.last_recv_seq_no = seq_no;
+        heartbeat_source.is_active = true;
+        heartbeat_source.last_heartbeat_time = std::chrono::system_clock::now();
 
-// periodically check to see if nodes have expired due to lack of recent heartbeat packets from those nodes
-static void node_timeout_loop()
-{
-    // do not check self!!!
+        update_forwarding_table(this_node_addr);
 
+        return true;
+    }
+    else
+        return false;
 }
 
 // periodically send node heartbeats to adjacent nodes
 static void send_node_update_loop(const net::sock_fd & recv_sock_fd)
 {
+
+    // initialize UDP send socket
+    const net::sock_fd send_sock_fd(socket(AF_INET, SOCK_DGRAM, 0));
+    net::set_buffer_size(send_sock_fd.get());
+    if (send_sock_fd.get() < 0)
+    {
+        std::cerr << "Could not initialize send socket\n";
+        return;
+    }
+
     static uint32_t curr_seq_no{0};
+    sockaddr_in dest_addr{0};
+    dest_addr.sin_family = AF_INET;
 
     net::TransportPacket heartbeat(net::BASE_PACKET_TYPE::LINK, net::TransportPacket::TRANSPORT_PRIORITY::HIGH, *(sockaddr*)&this_node_addr, 
-        {0}, curr_seq_no); // These are intended as "broadcast" messages and therefore do not have a specific dest address
+        *(sockaddr*)&dest_addr, curr_seq_no); // These are intended as "broadcast" messages and therefore do not have a specific dest address
 
     while (true)
     {
@@ -324,14 +347,16 @@ static void send_node_update_loop(const net::sock_fd & recv_sock_fd)
         // mutex for socket
         {
             std::lock_guard<std::mutex> lock(socket_mtx);
-
+            for (const auto & node : adjacent_nodes)
+            {
+                heartbeat.forward_packet(recv_sock_fd,*(sockaddr*)&node);
+                std::cout << "Sending heartbeat to: " << node << '\n';
+            }
         }
-
     }
 }
 
-static void routing_loop(const forwarding_table & ft, const net::sock_fd & recv_sock_fd, const sockaddr & router_addr,
-    const uint32_t queue_max_size)
+static void routing_loop(const net::sock_fd & recv_sock_fd, const sockaddr & router_addr)
 {
     std::array<uint8_t, net::RECV_BUFFER_SIZE> recv_buf;
 
@@ -361,7 +386,17 @@ static void routing_loop(const forwarding_table & ft, const net::sock_fd & recv_
             // Check if this is a node-state packet
             if (recv_packet.get_base_type() == net::BASE_PACKET_TYPE::LINK)
             {
-                process_node_state(recv_packet.get_transport_src(), recv_packet.get_seq_no());
+                if (process_node_state(recv_packet.get_transport_src(), recv_packet.get_seq_no()))
+                {
+                    // if signal iteration not already received, propagate heartbeat signal to adjacent nodes (other than the one that just sent it)
+                    for (const auto & node : adjacent_nodes)
+                    {
+                        if (!(node == *(sockaddr_in*)&src_addr))
+                        {
+                            recv_packet.forward_packet(recv_sock_fd, *(sockaddr*)&node);
+                        }
+                    }
+                }
                 continue;
             }
 
@@ -399,6 +434,18 @@ static void routing_loop(const forwarding_table & ft, const net::sock_fd & recv_
             }    
             outgoing_queue.pop();
         }
+        const auto curr_time = std::chrono::system_clock::now();
+        bool topology_changed{false};
+        for (auto & node : node_states)
+        {
+            if (node.second.is_active && node.second.last_heartbeat_time + NODE_EXPIRATION_INTERVAL < curr_time)
+            {
+                node.second.is_active = false;
+                topology_changed = true;
+            }
+        }
+        if (topology_changed)
+            update_forwarding_table(this_node_addr);
     }
 }
 
@@ -443,13 +490,21 @@ int main(int argc, char * argv[])
             return -1;
         }
 
-        //const auto recv_fd_addr = net::bind_recv_local(emulator_port);
-        this_node_addr = net::get_sockaddr_in_from_hostport("localhost",emulator_port);
+        const auto recv_fd_addr = net::bind_recv_local(emulator_port);
+        this_node_addr = *(sockaddr_in*)&recv_fd_addr.second;
         std::cout << "Localhost found at: " << this_node_addr << "\n";
         auto top = parse_topology(top_file);
-        std::cout << "\nTopology for this node:\n"; print_topology(top,std::cout);
+        get_adj_nodes(this_node_addr, adjacent_nodes);
 
-        //routing_loop(top,recv_fd_addr.first,recv_fd_addr.second);
+        std::cout << "\nTopology for this node:\n"; 
+        print_topology(top,std::cout);
+
+        std::thread th(send_node_update_loop, std::ref(recv_fd_addr.first));
+
+        update_forwarding_table(this_node_addr);
+        routing_loop(recv_fd_addr.first, recv_fd_addr.second);
+
+        th.join();
     }
     catch (std::runtime_error & e)
     {
